02446b1cd1b23a6c710703c604720925407ddfc3
JWeyhric <jweyhrichrl@gmail.com>
2026-06-02 02:04:09 -0700

    Add files via upload

---
+++ Data/winners.js
@@ -0,0 +1,62 @@
+const winnersData = {
+    miami: [
+        {
+            year: 2026,
+            driver: "Kimi Antonelli",
+            team: "Mercedes",
+            color: "#27F4D2"
+        }
+    ],
+    canada: [
+        {
+            year: 2026,
+            driver: "Kimi Antonelli",
+            team: "Mercedes",
+            color: "#27F4D2"
+        },
+        {
+            year: 2025,
+            driver: "George Russell",
+            team: "Mercedes",
+            color: "#27F4D2"
+        },
+        {
+            year: 2024,
+            driver: "Max Verstappen",
+            team: "Red Bull",
+            color: "#3671c6"
+        },
+        {
+            year: 2023,
+            driver: "Max Verstappen",
+            team: "Red Bull",
+            color: "#3671c6"
+        },
+        {
+            year: 2022,
+            driver: "Max Verstappen",
+            team: "Red Bull",
+            color: "#3671c6"  
+        }
+    ],
+    monaco: [
+        {
+            year: 2025,
+            driver: "Lando Norris",
+            team: "McLaren",
+            color: "#FF8000"
+        },
+        {
+            year: 2024,
+            driver: "Charles Leclerc",
+            team: "Ferrari",
+            color: "#E8002D"
+        },
+        {
+            year: 2023,
+            driver: "Max Verstappen",
+            team: "Red Bull",
+            color: "#3671c6"
+        }
+    ]
+}
