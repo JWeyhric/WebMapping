e874e6076f739319ae19a6873a7b22313188ce54
JWeyhric <jweyhrichrl@gmail.com>
2026-06-09 18:10:08 -0700

    Update F1Main.js

--- F1Main.js
+++ F1Main.js
@@ -1,32 +1,43 @@
-
 // mapbox access token
 mapboxgl.accessToken = 'pk.eyJ1IjoiandleWhyaWMiLCJhIjoiY21udjM1aXAyMTZmZDJxb2FmaTl0bzI1cyJ9.u2_BfMUxO3b4Mrbq8b0goA';
 
-miami_bounds = [
-  [-80.2455, 25.9525], // southwest corner
-  [-80.2265, 25.9635] // northeast corner
-]
-
+// Initiate Map
 var testmap = new mapboxgl.Map({
     container: 'testmap', // id of the previously made div
     style: 'mapbox://styles/jweyhric/cmonb9b8i003z01ssc2zo4d70', // custom style
-    center: [-73.5261, 45.5050], // starting position
+    center: [-73.5261, 45.5055], // starting position
     zoom: 14.5, // starting zoom
-    minZoom: 9,
-    //maxBounds: miami_bounds
+    minZoom: 1, 
+    // in order to allow cinematic to zoom from far out
+    // max bounds will stop user from leaving/zooming too far out
+    // in order to make map controls not zoom too far out each track will have a minZoom updating each switch
 });
+
+// Disable right click rotation
+testmap.dragRotate.disable();
+
+// Disable touch-rotation
+testmap.touchZoomRotate.disableRotation();
+
+// Create map controls
+testmap.addControl(new mapboxgl.NavigationControl(), 'bottom-left');
 
 // GLOBAL TRACK VARIABLE
 let currentTrack = null;
 
-// tracks
+// create tracks object for each track, calling the file, poifile, and giving useful information
 const tracks = [
   {
     id: "canada",
     file: "Data/Tracks/ca-1978.geojson",
     poiFile: "Data/POI/canada_poi.geojson",
-    center: [-73.5261, 45.5050], 
-    zoom: 14.5,
+    center: [-73.5261, 45.5055], 
+    zoom: 15,
+    minZoom: 14,
+    bounds: [
+      [-73.56610, 45.48337],          // half-screen [-73.54500, 45.48500], // southwest corner
+      [-73.48996, 45.52451]          // half-screen [-73.51210, 45.51900] // northeast corner
+    ],
     color: "#000000"
   },
   {
@@ -35,6 +46,10 @@ const tracks = [
     poiFile: "Data/POI/monaco_poi.geojson",
     center: [7.42495, 43.73688], 
     zoom: 15.5,
+    bounds: [
+      [7.40317, 43.72500],          // half-screen [7.41317, 43.73004], // southwest corner
+      [7.44731, 43.75000]          // half-screen [7.43731, 43.74041] // northeast corner
+    ],
     color: "#000000"
   },
   {
@@ -70,7 +85,7 @@ document.getElementById("initial-popup-closebtn").addEventListener("click", func
 testmap.on('load', function () {
 
   tracks.forEach(track => {
-    // Add track source
+    // Get track source
     testmap.addSource(`${track.id}-track`, {
       type: "geojson",
       data: track.file
@@ -81,13 +96,45 @@ testmap.on('load', function () {
       id: `${track.id}-line`,
       type: "line",
       source: `${track.id}-track`,
+      slot: "middle", // adds tracks under the 3D buildings layer to prevent track overlaying on top of buildings
+      minzoom: 11, // min zoom where track is visible
       layout: {
         'line-join': 'round',
         'line-cap': 'round'
       },
       paint: {
+        'line-opacity': [
+          'interpolate',
+          ['linear'],
+          ['zoom'],
+
+          11, 0.25,  // zoom 11, opacity 0
+          12, 0.5,
+          13, 1
+        ],
         'line-color': track.color,
         'line-width': 7
+      }
+    });
+
+    // Add arrow for track direction
+    testmap.addLayer({
+      id: `${track.id}-direction-arrows`,
+      type: "symbol",
+      source: `${track.id}-track`,
+      slot: "middle", // same as track
+      layout: {
+        "symbol-placement": "line",
+        "symbol-spacing": 175,
+        "text-field": "\u25B6",
+        "text-size": 16,
+        "text-keep-upright": false,
+        "text-allow-overlap": true
+      },
+      paint: {
+        "text-color": "#e10600",
+        "text-halo-color": "#ffffff",
+        "text-halo-width": 2
       }
     });
  
@@ -98,31 +145,11 @@ testmap.on('load', function () {
       const polygonLayerID = `${track.id}-poi-polygons`
 
       // TEST for POI
-      console.log("loading POI for:", track.id, track.poiFile);
+      //console.log("loading POI for:", track.id, track.poiFile);
       
       testmap.addSource(`${track.id}-poi`, {
         type: 'geojson',
         data: track.poiFile
-      });
-      
-      // POI Polygon Layer
-      testmap.addLayer({
-        id: polygonLayerID,
-        type: 'fill',
-        source: poiSourceID,
-        paint: {
-          'fill-color': [
-            'match',
-            ['get', 'type'],
-            'grandstand', '#e10600',
-            'bathroom', '#1e90ff',
-            'food', '#ffaa00',
-            'paddock', '#7b2cbf',
-            '#333333'
-          ],
-          'fill-opacity': 0.4,
-          'fill-outline-color': '#000000'
-        }
       });
 
       // POI point layer
@@ -130,30 +157,47 @@ testmap.on('load', function () {
         id: pointLayerID,
         type: 'circle',
         source: poiSourceID,
+        slot: "middle", // same slot as track to avoid seeing points through buildings
+        minzoom: 14, // minimum zoom for visibility
         paint: {
-          'circle-radius': 5,
+          'circle-radius': [
+            'interpolate',
+            ['linear'],
+            ['zoom'],
+            14, 3,
+            18, 6 // zoom 18, radius 6
+          ],
           'circle-color': [
             'match',
             ['get', 'type'],
-            'history', '#ffd700',
+            'history', '#3b49c9', // #ffd700
             '#333333'
           ],
           'circle-stroke-width': 2,
-          'circle-stroke-color': '#ffffff'
+          'circle-stroke-color': '#ffffff',
+          'circle-opacity': 1,
+          'circle-stroke-opacity': 1
         }
       });
       // Make POIs Interactable
       [pointLayerID, polygonLayerID].forEach(layerID => {
         testmap.on("click", layerID, (e) => {
-          const props = e.features[0].properties;
+          const feature = e.features[0];
+          const props = feature.properties;
           //const coords = e.features[0].geometry.coordinates.slice();
+
+          console.log(props.type);
+          // zoom to point on click
+          if (props.type === "history") {
+            zoomHistory(feature);
+          }
+          
 
           new mapboxgl.Popup()
             .setLngLat(e.lngLat)
             .setHTML(`
-              <h3>${props.name}</h3>
+              <h3>${props.title}</h3>
               <p>${props.description}</p>
-              <p><strong>Type:</strong> ${props.type}</p>
               `)
             .addTo(testmap);
         });
@@ -172,10 +216,18 @@ testmap.on('load', function () {
   });
 });     
 
+/* TRACK SWITCHING FUNCTION */
 
-// EVEN MORE COMPLEX FLY TO
+// Function to animate switch between tracks
 function cinematicTrackJump(centerCoords, finalZoom) {
   const mapDiv = document.getElementById("testmap");
+
+  // Remove min zoom to make animation work
+  testmap.setMinZoom(1);
+  console.log(testmap.getMinZoom());
+
+  // remove scroll ability so user doesn't pause animation
+  testmap.scrollZoom.disable();
 
   // fade map out
   mapDiv.style.transition = "opacity 0.6s";
@@ -185,7 +237,7 @@ function cinematicTrackJump(centerCoords, finalZoom) {
     // instantly move (jump)
     testmap.jumpTo({
       center: centerCoords,
-      zoom: finalZoom - 1,
+      zoom: finalZoom - 12, // zoom from further out
       pitch: 0,
       bearing: 0
     });
@@ -198,55 +250,50 @@ function cinematicTrackJump(centerCoords, finalZoom) {
       // small local zoom-in animation
       testmap.easeTo({
         zoom: finalZoom,
-        duration: 1200
+        duration: 5000
+      });
+
+      testmap.once("moveend", () => {
+        testmap.setMinZoom(startingTrack.minZoom);  // Once animation is done, set the minimum zoom for track
+        testmap.scrollZoom.enable(); // re-enable scrolling
       });
     });
   }, 450);
 }
-
-// Add listeners for the buttons and Change Button Style
-tracks.forEach(track => {
-  const button = document.getElementById(track.id);
-
-  if(button) {
-    button.addEventListener("click", () => {
-      cinematicTrackJump(track.center, track.zoom);
-      
-      // reset buttons
-      document.querySelectorAll(".track-button button").forEach(btn => {
-        btn.classList.remove("active-track");
-      });
-
-      // Update clicked button
-      button.classList.add("active-track");
-    });
-
-    // Store original button name
-    button.dataset.originalName = button.textContent;
-  };
-});
 
 /* NAV */
 
 // Toggle Tracks Nav
 function toggleNav() {
   const nav = document.getElementById("trackSelect");
+  const controls = document.querySelector(".mapboxgl-ctrl-bottom-left");
 
   // Check for nav being open
   if(nav.style.width === "250px") {
     nav.style.width = "0";
+    controls.style.left = "0"; // move map controls back
   } else {
     nav.style.width = "250px";
+    controls.style.left = "250px"; // move map controls with sidebar
   }
 }
 
-// default track
+// STARTING POSITION
 const startingTrackID = "canada";
 
 const startingTrack = tracks.find(t => t.id === startingTrackID);
 if(startingTrack) {
   // Update Global Var
   currentTrack = startingTrack;
+
+  // Set bounds
+  testmap.setMaxBounds(startingTrack.bounds);
+
+  // Update Zoom
+  testmap.setZoom(startingTrack.zoom);
+
+  // Set Min Zoom
+  testmap.setMinZoom(startingTrack.minZoom);
 
   // Update history
   updateHistory(startingTrack);
@@ -278,6 +325,8 @@ document.querySelectorAll(".track-link").forEach(link => {
       // update global var
       currentTrack = track;
 
+      testmap.setMaxBounds(track.bounds); // set bounds
+
       cinematicTrackJump(track.center, track.zoom);
 
       document.querySelectorAll(".track-link").forEach(item => {
@@ -289,7 +338,7 @@ document.querySelectorAll(".track-link").forEach(link => {
       document.getElementById("currentTrackName").textContent =
       trackID.charAt(0).toUpperCase() + trackID.slice(1);
       
-      closeNav(); // Closes dropdown
+      toggleNav(); // Closes dropdown
       closeHistory();
       updateHistory(track); // Updated history for track
     }
@@ -330,13 +379,15 @@ function updateHistory(track) {
   //historyTitle.textContent = track.name + "History";
   historyItems.innerHTML = "<h2>Important Track Features</h2>";
 
+  const winButton = document.getElementById("win-button");
+  winButton.innerHTML = "Historical Winners <span class=arrow>&#9660;</span>";
   // Get POI Data
   //const poiData = testmap.getSource(`${track.id}-poi`)._data;
 
   // checking if track has POI-FILE
   if(!track.poiFile) {
     historyItems.innerHTML = "<p>No history points - No Data</p>";
-    checkWinners(track);
+    checkWinners(track); // if no POI File still need to check for winners
     return;
   }
 
@@ -344,15 +395,6 @@ function updateHistory(track) {
   fetch(track.poiFile)
     .then(response => response.json())
     .then(poiData => {
-
-     /* const trackFeature = poiData.features.filter(feature =>
-        feature.properties.type === "trackFeature"
-      );
-
-      const outcomes = poiData.features.filter(feature =>
-        feature.properties.type === "outcome"
-      );
-      */
 
       const historyPoints = poiData.features.filter(feature =>
         feature.properties.type === "history"
@@ -366,25 +408,15 @@ function updateHistory(track) {
       item.textContent = feature.properties.title;
 
       item.addEventListener("click", () => {
-        const coords = feature.geometry.coordinates;
-        const props = feature.properties;
-
-        testmap.easeTo({
-          center: coords,
-          zoom: props.zoom || 17,
-          pitch: props.pitch || 60,
-          bearing: props.bearing || 0,
-          duration: 1800
-        });
-
-        document.getElementById("currentTrackName").classList.add("track-return-enabled");
-
+        zoomHistory(feature);
       });
       historyItems.appendChild(item);
     });
   });
 
+
   /* WINNERS */
+  checkWinners(track);
   
   const winnerItems = document.getElementById("winnerItems");
 
@@ -408,6 +440,23 @@ function updateHistory(track) {
       winnerItems.appendChild(card);
     });
   }
+}
+
+/* Reusable Zooming function for History Points*/
+function zoomHistory(feature) {
+  const coords = feature.geometry.coordinates;
+  const props = feature.properties;
+
+  testmap.easeTo({
+    center: coords,
+    zoom: props.zoom || 17,
+    pitch: props.pitch || 60,
+    bearing: props.bearing || 0,
+    duration: 1800
+  });
+
+  // allow user to click track name on top to return to original view of track
+  document.getElementById("currentTrackName").classList.add("track-return-enabled");
 }
 
 /* NO POI FILE CHECK FOR WINNERS */
